d7d3cbf817bc03ebaffe54c07080c3fa9f2a09a8
oksanaliudukhovska <aldz84@ukr.net>
2024-08-24 09:13:25 +0200

    first commit

---
+++ task1.js
@@ -0,0 +1,61 @@
+
+
+
+
+// let pinside = document.querySelector(".answer")
+// let cyrcle = document.querySelector("#circle");
+// console.dir(cyrcle);
+
+// cyrcle.addEventListener('click', showMessage)
+  
+//   function showMessage(){
+//     let number = Math.floor(Math.random()*10 +1);
+//      console.log(number)
+    
+//    switch (number) {
+//     case 1:{
+//     console.log('да');
+//     pinside.innerHTML = "Yes - definitely";}
+//     break;
+//     case 2:{
+//     console.log('конечно');
+//     pinside.innerHTML = "It is decidedly";}
+//     break;
+//     case 3:{    
+//     console.log('бесспорно');
+//     pinside.innerHTML = "It is certain";}
+//     break;
+//     case 4:{
+//     console.log('должно быть так');
+//     pinside.innerHTML = "Signs point to yes";}
+//     break;
+//     case 5:{
+//     console.log('возможно');
+//     pinside.innerHTML = "Most likely";}
+//     break;
+//     case 6:{
+//     console.log('шансов мало');
+//     pinside.innerHTML = "Outlook not so good";}
+//     break;
+//     case 7:{
+//     console.log('нет');
+//     pinside.innerHTML = "Don't count on it";}
+//     break;
+//     case 8:{
+//     console.log('звезды говорят нет');
+//     pinside.innerHTML = "My sources say no";}
+//     break;
+//     case 9:{
+//     console.log('не могу сказать');
+//     pinside.innerHTML = "Reply hazy, try again";}
+//     break;
+//     case 10:{
+//     console.log('сейчас неизвестно');
+//     pinside.innerHTML = "Cannot predict now";}
+//     break;
+//     case 11:{
+//     console.log('спросите позже');
+//     pinside.innerHTML = "Ask me later";}
+//     break;
+//    }      
+// }
